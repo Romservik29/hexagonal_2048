a38c2e01448a55ab6eb09cd0f6913f056c100448
romservik29 <andrei.romservik29@gmail.com>
2021-03-14 08:54:14 +0300

    draw grid if #test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
 import { addCeelsOnMap, generateMap, mapToColumns, notEmptyCeels } from './app/mapGenerator'
 import { Container } from './components/Container'
 import Grid from './components/Grid'
 
-export default function App() {
+export default function App(props) {
     const [radius, setRadius] = useState(0)
     const [map, setMap] = useState(0)
-
+    const [isGameOver, setIsGameOver] = useState(false);
+    let location = useLocation();
+    useEffect(() => {
+        console.log(location.hash)
+        radiusClick(+location.hash.slice(-1))
+    }, [])
     const radiusClick = (e) => {
         setRadius(e)
         let newMap = generateMap(e)
@@ -20,13 +26,23 @@ export default function App() {
     }
 
     return (
-        <Container>
-            <button onClick={() => radiusClick(2)}>2</button>
-            <button onClick={() => radiusClick(3)}>3</button>
-            <button onClick={() => radiusClick(4)}>4</button>
-            {radius}
-            {radius != 0 ? <Grid radius={radius} map={map} setMap={setMap} /> : false}
+        <div>
+            <div style={{textAlign: 'center'}}>
+                <button onClick={() => radiusClick(2)}>2</button>
+                <button onClick={() => radiusClick(3)}>3</button>
+                <button onClick={() => radiusClick(4)}>4</button>
+                {isGameOver ? (
+                    <div>
+                        Game Status: <span data-status="playing">playing</span>
+                    </div>
+                ) : (
+                    <div>
+                        Game Status: <span data-status="playing">playing</span>
+                    </div>
+                )}
+            </div>
 
-        </Container>
+            {radius != 0 ? <Grid radius={radius} map={map} setMap={setMap} /> : false}
+        </div>
     )
 }
