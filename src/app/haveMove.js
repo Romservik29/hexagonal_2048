3707427a2731ab0bab27dd2f01731ac3ad25e995
romservik29 <andrei.romservik29@gmail.com>
2021-03-15 10:24:44 +0300

    Up ready

--- src/app/haveMove.js
+++ src/app/haveMove.js
@@ -9,6 +9,24 @@ export function isMoveXUp(columns) {
                 if (hex.value !== 0 ? hex.value === cell.value || cell.value === 0 : false) status = true
         }
         else if (hex.x > 0 ? cell.x === hex.x : false) {
+            if (hex.y < cell.y) {
+                if (hex.value !== 0 ? hex.value === cell.value || cell.value === 0 : false) status = true
+            }
+        }
+
+    }))))
+    return status
+}
+
+export function isMoveXDown(columns) {
+    let status = false
+    let map = [...columns]
+    columns.some(column => column.some(hex => map.some(mapColumn => mapColumn.some(cell => {
+        if (hex.x <= 0 ? cell.x === hex.x : false) {
+            if (hex.y > cell.y)
+                if (hex.value !== 0 ? hex.value === cell.value || cell.value === 0 : false) status = true
+        }
+        else if (hex.x > 0 ? cell.x === hex.x : false) {
             if (hex.y > cell.y) {
                 if (hex.value !== 0 ? hex.value === cell.value || cell.value !== 0 : false) status = true
             }
@@ -17,7 +35,7 @@ export function isMoveXUp(columns) {
     }))))
     return status
 }
-
+/*
 export function isMoveXDown(columns) {
     let status = false
     let map = [...columns]
@@ -34,4 +52,4 @@ export function isMoveXDown(columns) {
 
     }))))
     return status
-}
+}*/
