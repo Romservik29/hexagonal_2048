4f2e1e65ca4e50ac9e1e2bba3d67927b005a6bb0
romservik29 <andrei.romservik29@gmail.com>
2021-03-19 06:22:10 +0300

    rework move and test for them

---
+++ src/move.js
@@ -0,0 +1,14 @@
+export default function move(map, radius, vector, up = true) {
+        for (let i = -radius + 1; i < radius; i++) {
+            let column = map.filter(hex => hex[vector] === i)
+            !up ? column.reverse() :false;
+            for (let j = 0; j < column.length - 1; j++) {
+                if (column[j].value === 0 ? column[j + 1].value !== 0 : false) {
+                    column[j].value = column[j + 1].value
+                    column[j + 1].value = 0
+                    j = -1
+                }
+            }
+        }
+    return map
+}
---
+++ src/move.test.js
@@ -0,0 +1,133 @@
+import move from "./move"
+
+test('moveXUp', () => {
+    const map = [
+        { x: -1, y: 1, z: 0, value: 0 },
+        { x: -1, y: 0, z: 1, value: 2 },
+        { x: 0, y: 1, z: -1, value: 0 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 2 },
+        { x: 1, y: 0, z: -1, value: 0 },
+        { x: 1, y: -1, z: 0, value: 2 }
+    ]
+    const map2 =
+        [{ x: -1, y: 1, z: 0, value: 2 },
+        { x: -1, y: 0, z: 1, value: 0 },
+        { x: 0, y: 1, z: -1, value: 2 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 0 },
+        { x: 1, y: 0, z: -1, value: 2 },
+        { x: 1, y: -1, z: 0, value: 0 }
+        ]
+    expect(move(map, 2, 'x')).toEqual(map2)
+})
+test('moveXDown', () => {
+    const map =
+        [{ x: -1, y: 1, z: 0, value: 2 },
+        { x: -1, y: 0, z: 1, value: 0 },
+        { x: 0, y: 1, z: -1, value: 2 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 0 },
+        { x: 1, y: 0, z: -1, value: 2 },
+        { x: 1, y: -1, z: 0, value: 0 }
+        ]
+    const map2 = [
+        { x: -1, y: 1, z: 0, value: 0 },
+        { x: -1, y: 0, z: 1, value: 2 },
+        { x: 0, y: 1, z: -1, value: 0 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 2 },
+        { x: 1, y: 0, z: -1, value: 0 },
+        { x: 1, y: -1, z: 0, value: 2 }
+    ]
+
+    expect(move(map, 2, 'x', false)).toEqual(map2)
+})
+test('moveYUp', () => {
+    const map =
+        [{ x: -1, y: 1, z: 0, value: 2 },
+        { x: -1, y: 0, z: 1, value: 2 },
+        { x: 0, y: 1, z: -1, value: 0 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 2 },
+        { x: 1, y: 0, z: -1, value: 0 },
+        { x: 1, y: -1, z: 0, value: 0 }
+        ]
+    const map2 = [
+        { x: -1, y: 1, z: 0, value: 0 },
+        { x: -1, y: 0, z: 1, value: 0 },
+        { x: 0, y: 1, z: -1, value: 2 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 0 },
+        { x: 1, y: 0, z: -1, value: 2 },
+        { x: 1, y: -1, z: 0, value: 2 }
+    ]
+
+    expect(move(map, 2, 'y', false)).toEqual(map2)
+})
+test('moveYDown', () => {
+    const map2 =
+        [{ x: -1, y: 1, z: 0, value: 2 },
+        { x: -1, y: 0, z: 1, value: 2 },
+        { x: 0, y: 1, z: -1, value: 0 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 2 },
+        { x: 1, y: 0, z: -1, value: 0 },
+        { x: 1, y: -1, z: 0, value: 0 }
+        ]
+    const map =
+        [
+            { x: -1, y: 1, z: 0, value: 0 },
+            { x: -1, y: 0, z: 1, value: 0 },
+            { x: 0, y: 1, z: -1, value: 2 },
+            { x: 0, y: 0, z: 0, value: 0 },
+            { x: 0, y: -1, z: 1, value: 0 },
+            { x: 1, y: 0, z: -1, value: 2 },
+            { x: 1, y: -1, z: 0, value: 2 }
+        ]
+
+    expect(move(map, 2, 'y')).toEqual(map2)
+})
+test('moveZUp', () => {
+    const map = [
+        { x: -1, y: 1, z: 0, value: 0 },
+        { x: -1, y: 0, z: 1, value: 0 },
+        { x: 0, y: 1, z: -1, value: 0 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 2 },
+        { x: 1, y: 0, z: -1, value: 2 },
+        { x: 1, y: -1, z: 0, value: 2 }
+    ]
+    const map2 =
+        [{ x: -1, y: 1, z: 0, value: 2 },
+        { x: -1, y: 0, z: 1, value: 2 },
+        { x: 0, y: 1, z: -1, value: 2 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 0 },
+        { x: 1, y: 0, z: -1, value: 0 },
+        { x: 1, y: -1, z: 0, value: 0 }
+        ]
+    expect(move(map, 2, 'z')).toEqual(map2)
+})
+test('moveZDown', () => {
+    const map =
+        [{ x: -1, y: 1, z: 0, value: 2 },
+        { x: -1, y: 0, z: 1, value: 2 },
+        { x: 0, y: 1, z: -1, value: 2 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 0 },
+        { x: 1, y: 0, z: -1, value: 0 },
+        { x: 1, y: -1, z: 0, value: 0 }
+        ]
+    const map2 = [
+        { x: -1, y: 1, z: 0, value: 0 },
+        { x: -1, y: 0, z: 1, value: 0 },
+        { x: 0, y: 1, z: -1, value: 0 },
+        { x: 0, y: 0, z: 0, value: 0 },
+        { x: 0, y: -1, z: 1, value: 2 },
+        { x: 1, y: 0, z: -1, value: 2 },
+        { x: 1, y: -1, z: 0, value: 2 }
+    ]
+
+    expect(move(map, 2, 'z', false)).toEqual(map2)
+})
